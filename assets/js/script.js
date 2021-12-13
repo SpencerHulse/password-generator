529e9994791afee28d387203698fb53f03afb165
Spencer Hulse <hulse.spencer@gmail.com>
2021-12-13 15:27:26 -0500

    added passwordLength function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
 // Assignment code here
+let generatePassword = function () {
+  let password = "";
 
+  //function for getting the length of the password
+  let length = passwordLength();
+  console.log("The length of the password is " + length + " characters.");
+}
+
+let passwordLength = function() {
+  let characters = "";
+  
+  //a validation loop to ensure a proper character length is entered
+  while (!characters || characters < 8 || characters > 128) {
+    //prompt the user to enter how many characters they want their password to be
+    characters = window.prompt("How many characters do you want your password to be? You can choose anywhere between 8 and 128 characters!");
+
+    //converting the string from the prompt into an integer
+    characters = parseInt(characters);
+    console.log(characters.length);
+    console.log(!characters)
+
+    //check for valid character length and either recursively enter the function or return the characters
+    if (!characters || characters < 8 || characters > 128) {
+      window.alert("You have entered an invalid character length. Please try again!");
+      generatePassword();
+    } else {
+      return characters;
+    }
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
