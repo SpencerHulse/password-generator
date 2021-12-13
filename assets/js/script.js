2e69364c9356a1ff526798cec2a6678ad3429aac
Spencer Hulse <hulse.spencer@gmail.com>
2021-12-13 15:50:01 -0500

    added characterTypes function and alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ let generatePassword = function () {
   //function for getting the length of the password
   let length = passwordLength();
   console.log("The length of the password is " + length + " characters.");
+
+  //alert and function for choosing the types of characters to be included
+  window.alert("Please choose the types of character you want to include in your password. You must choose at least one.");
+  let characterTypeArray = characterTypes();
+  console.log("The characters included are " + characterTypeArray.join(""));
+  console.log("The number of characters selected is " + characterTypeArray.length);
 }
 
 let passwordLength = function() {
@@ -30,24 +36,55 @@ let passwordLength = function() {
   }
 }
 
-let characterTypeObj = [
-  {
-    name: "lowercase",
-    array: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  },
-  {
-    name: "uppercase",
-    array: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  },
-  {
-    name: "numeric",
-    array: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-  },
-  {
-    name: "special",
-    array: [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+let characterTypes = function() {
+  //object holding the various types of characters and their arrays
+  let characterTypeObj = [
+    {
+      name: "lowercase",
+      array: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    },
+    {
+      name: "uppercase",
+      array: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    },
+    {
+      name: "numeric",
+      array: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    },
+    {
+      name: "special",
+      array: [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+    }
+  ];
+
+  //the array that will hold all selected character types
+  let selectedArray = [];
+  let chosenTypes = [];
+
+  //the loop used to select character types
+  for(let i = 0; i < characterTypeObj.length; i++) {
+    let confirmCharacterType = window.confirm("Do you want your password to include " + characterTypeObj[i].name + " letters?");
+    
+    //asks whether they want each type of character
+    if (confirmCharacterType) {
+      selectedArray = selectedArray.concat(characterTypeObj[i].array);
+      chosenTypes = chosenTypes.concat(characterTypeObj[i].name);
+    }
   }
-];
+
+  //checks to make sure at least one type was selected
+  if (selectedArray.length === 0) {
+    window.alert("You did not select any character types. Please choose again.");
+    characterTypes();
+  } else {
+    let typesConfirm = window.confirm("You have chosen these character types: " + chosenTypes.join(", ") + "." + "\n\nIs that correct?");
+
+    if(!typesConfirm) {
+      characterTypes();
+    }
+  }
+  return selectedArray;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
